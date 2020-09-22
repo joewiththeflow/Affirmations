7d0490df719fe4f07e4321d4516dc1e72b6d6cfd
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2020-09-22 21:34:34 +0100

    Added id to textView on activity_main.xml Used this to check loading DataSource

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.affirmations
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = DataSource().loadAffirmations().size.toString()
+
     }
 }
